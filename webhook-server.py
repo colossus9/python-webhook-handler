9e5b8a406dbc7deac66d6b62f32f2cf490763017
James Garcia <colossus9@github.com>
2017-12-12 23:23:43 -0600

    Trying dynamic method calling

--- webhook-server.py
+++ webhook-server.py
@@ -5,20 +5,38 @@ app = Flask(__name__)
 
 @app.route('/webhook-server', methods=['GET', 'POST'])
 def webhookServer():
+
+    # Let's go ahead and make GET requests happy. You could use it to test firewall rules
     if request.method == 'GET':
         return jsonify({'status':'success'}), 200
 
+    # Webhooks will make POST requests
     elif request.method == 'POST':
-        print ' '
-        print(json.dumps(request))
-        print ' '
-        print(json.dumps(request.json))
-        print ' '
 
-        return '', 200
+        # Debugging output
+        print '======= DEBUG: BEGIN REQUEST JSON ======='
+        print(json.dumps(request.json))
+        print '======= DEBUG: END REQUEST JSON ======='
+
+        # Let's get the webhook event so we know what happened
+        event = request.headers.get('X-GitHub-Event')
+
+        # Perform actions based on the event that occurred. Events are defined at:
+        # https://developer.github.com/v3/activity/events/types/
+        #if event == "ping":
+        #    return jsonify({'event':'ping','status':'success'}), 200
+        #elif event == "repository":
+        #    return jsonify({'event':'repository','status':'success'}), 200
+        #elif event == "create":
+        #    return jsonify({'event':'create','status':'success'}), 200
+
+        getattr(sys.modules[__name__], str(event)()  # Call the method based on the event type
 
     else:
         abort(400)
+
+def ping():
+    return jsonify({'event':'ping','status':'success'}), 200
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0')  # Run on the machine's IP address and not just localhost
