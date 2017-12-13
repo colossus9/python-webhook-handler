4b8ffe382e06c9aeb640cdd71b48ca476dfd0abf
James Garcia <colossus9@github.com>
2017-12-13 02:31:54 -0600

    Fixed typo

--- webhook-server.py
+++ webhook-server.py
@@ -62,7 +62,7 @@ def webhookServer():
 
                 elif EVENT == "repository":
                     print 'Event Action: ' + request.json["action"]
-                    debugPrintWebhookJSON(request.json
+                    debugPrintWebhookJSON(request.json)
                     orgRepo = request.json["repository"]["full_name"]
                     endpoint = 'https://ec2-35-164-144-23.us-west-2.compute.amazonaws.com/api/v3/teams/7/repos/' + orgRepo + '?permission=admin'
                     r = requests.put(endpoint, data = None)
