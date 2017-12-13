e2be732c46dc59702a06832d7a306aced2e3c4a3
James Garcia <colossus9@github.com>
2017-12-12 23:00:33 -0600

    Debugging request

--- webhook-server.py
+++ webhook-server.py
@@ -2,13 +2,14 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-
 @app.route('/webhook-server', methods=['GET', 'POST'])
 def webhookServer():
     if request.method == 'GET':
         return jsonify({'status':'success'}), 200
 
     elif request.method == 'POST':
+        print ' '
+        print(request)
         print ' '
         print(request.json)
         print ' '
@@ -17,7 +18,6 @@ def webhookServer():
 
     else:
         abort(400)
-
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0')  # Run on the machine's IP address and not just localhost
