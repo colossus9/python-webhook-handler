0a3ea5ac6eaa5dc6a2a3e1d6908b5495c21a6029
James Garcia <colossus9@github.com>
2017-12-13 00:02:43 -0600

    Debugging info

--- webhook-server.py
+++ webhook-server.py
@@ -6,17 +6,28 @@ app = Flask(__name__)
 @app.route('/webhook-server', methods=['GET', 'POST'])
 def webhookServer():
 
+    # Define vars
+    GHE_ADDRESS = os.environ.get('GHE_ADDRESS', None)  # The address of the GitHub Enterprise server
+
     # Let's go ahead and make GET requests happy. You could use it to test firewall rules
     if request.method == 'GET':
-        return jsonify({'status':'success'}), 200
+        return jsonify({'method':'GET','status':'success'}), 200
 
     # Webhooks will make POST requests
     elif request.method == 'POST':
 
         # Debugging output
+        print ' '
+        print '======= DEBUG: ENVIRONMENT ======='
+        print 'url=' + request.url
+        print 'base_url=' + request.base_url
+        print 'url_root=' + request.url_root
+        print 'data=' + request.data
+        print 'headers=' + json.dumps(request.url)
         print '======= DEBUG: BEGIN REQUEST JSON ======='
         print(json.dumps(request.json))
         print '======= DEBUG: END REQUEST JSON ======='
+        print ' '
 
         # Let's get the webhook event so we know what happened
         event = request.headers.get('X-GitHub-Event')
@@ -28,7 +39,7 @@ def webhookServer():
 
         elif event == "repository":
             return jsonify({'event':'repository','status':'success'}), 200
-        
+
         elif event == "create":
             return jsonify({'event':'create','status':'success'}), 200
 
